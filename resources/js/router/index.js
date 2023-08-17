fe70a18ef09c8b02d07e93c5a5f695be734e400f
ivan.taranenko <vanokalipsi@gmail.com>
2023-08-17 15:56:27 +0300

    first commit

---
+++ resources/js/router/index.js
@@ -0,0 +1,14 @@
+import {createRouter, createWebHistory} from "vue-router";
+import Index from '../components/Index.vue'
+
+
+const routes = [
+    {path: "/", name: "Index", component: Index},
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
